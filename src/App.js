682894f6dfa94a3b18c26929e62563ea608dbc4b
Pavel Bouša <pavel.bousa89@gmail.com>
2023-05-16 16:02:59 +0200

    Search app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react"
+import allMovies from "./data"
 
-function App() {
+const App = () => {
+    const [searchingText, setSearchingText] = useState("")
+    const [filteredMovies, setFilteredMovies] = useState([])
+
+    useEffect(() => {
+        const moviesAfterFilter = allMovies.filter((oneMovie) => {
+            return oneMovie.title.toLowerCase().includes(searchingText.toLowerCase())
+        })
+        setFilteredMovies(moviesAfterFilter)
+
+    }, [searchingText])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="movies-box" >
+        <form>
+            <input type="text" placeholder="Hledaný film" onChange={(e) => setSearchingText(e.target.value)} />
+        </form>
+        <div className="all-movies" >
+              {filteredMovies.map((oneMovie) => {
+                const {id, image, title, age, tags, description} = oneMovie
+
+                return <div key={id} className="one-movie" >
+                    <img src={image} alt="" />
+                    <h2>{title}</h2>
+                    <p>{age}</p>
+                    <p>{tags}</p>
+                    <p>{description}</p>
+                </div>
+            })}
+        </div>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
